c75a17361fc663668eeca549f92bcd36524157c5
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-06-12 04:48:01 +0600

    delete user added

--- index.js
+++ index.js
@@ -79,6 +79,14 @@ async function run() {
       res.send(result);
     })
 
+    
+    app.delete('/delete/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await studentsCollection.deleteOne(query);
+      res.send(result);
+  })
+
 
 
 
