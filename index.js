d4ee43ac2fb5b43836dd00a80296fd48b2a1583e
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-06-12 20:48:04 +0600

    changes

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const express= require('express')
 const cors= require('cors')
 
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
+// const stripe=require('stripe')(process.env.PAYMENT_KEY)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express();
 const port= process.env.PORT || 5000  
@@ -86,6 +87,19 @@ async function run() {
       const result = await studentsCollection.deleteOne(query);
       res.send(result);
   })
+
+  // app.post('/payment-intent',async(req,res)=>{
+  //   const {price}=req.body;
+  //   const amount=price*100;
+  //   const paymentIntent=await stripe.paymentIntents.create({
+  //     amount : amount,
+  //     currency: 'usd',
+  //     payment_method_types: ['card']
+  //   })
+  //   res.send({
+  //     clientSecret: paymentIntent.client_secret
+  //   })
+  // })
 
 
 
